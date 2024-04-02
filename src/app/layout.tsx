45a566cb70164ab8051761094001bafda4850e57
igor-mlv <malov7704@gmail.com>
2024-04-01 20:16:15 -0400

    favicon added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Bathroom Renovation",
   description: "Renovate your bathroom with our help",
+  icons: {
+    icon: 'assets/favicon.svg',
+  },
 };
 
 export default function RootLayout({
